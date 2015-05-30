d7071f319d740d48a9d5ff668eb3b18b32d9ccba
c9dong <davidCdong_123@hotmail.com>
2015-05-30 18:41:25 -0400

    camera change

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,17 +11,18 @@ public class CameraScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.RightArrow)){
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += Vector3.left * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.position += Vector3.up * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow)){
-			transform.position -= Vector3.up * speed * Time.deltaTime;
-		}
+		Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y + Mathf.Pow ((Time.time / 600.0f), 2), Camera.main.transform.position.z);
+//		if (Input.GetKey(KeyCode.RightArrow)){
+//			transform.position += Vector3.right * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey(KeyCode.LeftArrow)){
+//			transform.position += Vector3.left * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey(KeyCode.UpArrow)){
+//			transform.position += Vector3.up * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey(KeyCode.DownArrow)){
+//			transform.position -= Vector3.up * speed * Time.deltaTime;
+//		}
 	}
 }
