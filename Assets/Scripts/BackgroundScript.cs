be021d5b30db4c4205aa2a733ae5efb113bd2c36
c9dong <davidCdong_123@hotmail.com>
2015-05-30 14:21:38 -0400

    working infinite scroll

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -12,7 +12,29 @@ public class BackgroundScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (currentBackground);
+		//Debug.Log (currentBackground.transform.position.y + " " + Camera.main.transform.position.y);
+		if (!isVisible (currentBackground)) {
+			Debug.Log (currentBackground);
+			Vector2 newPosition = new Vector2(currentBackground.transform.position.x, currentBackground.transform.position.y + 48);
+			currentBackground.transform.position = newPosition;
+			currentBackground = nextObject();
+		}
 	}
 
+	bool isVisible(GameObject gameObject) {
+		float cameraOffset = Camera.main.transform.position.y;
+		float objectOffset = gameObject.transform.position.y;
+		if (cameraOffset - objectOffset >= 20) {
+			return false;
+		}
+		return true;
+	}
+
+	GameObject nextObject() {
+		if (currentBackground == transform.Find ("background1").gameObject) {
+			return transform.Find ("background2").gameObject;
+		} else {
+			return transform.Find ("background1").gameObject;
+		}
+	}
 }
